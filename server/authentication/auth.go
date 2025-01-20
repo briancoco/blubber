5ffe7ac720f4107966d8fdda21365d843f375d4a
briancoco <briannguyencoco@gmail.com>
2025-01-20 17:16:23 -0600

    user login

--- server/authentication/auth.go
+++ server/authentication/auth.go
@@ -12,6 +12,7 @@ import (
 	"os"
 
 	"github.com/go-chi/chi"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 type User struct {
@@ -22,9 +23,6 @@ type User struct {
 var AuthRouter *chi.Mux
 
 func registerUser(resp http.ResponseWriter, req *http.Request) {
-	// do error checking
-	// make sure that data is present in the request body and satisfies requirements
-	// then hash the password and persist to db (HMAC)
 	if req.Header.Get("Content-Type") != "application/json" {
 		resp.WriteHeader(400)
 		resp.Write([]byte("400 Bad Request"))
@@ -50,7 +48,7 @@ func registerUser(resp http.ResponseWriter, req *http.Request) {
 	//hash password
 	key := []byte(os.Getenv("SECRET_KEY"))
 	hmacEnc := hmac.New(sha256.New, key)
-	hmacEnc.Write(key)
+	hmacEnc.Write([]byte(user.Password))
 	user.Password = hex.EncodeToString(hmacEnc.Sum(nil))
 
 	//write to db
@@ -66,7 +64,61 @@ func registerUser(resp http.ResponseWriter, req *http.Request) {
 
 }
 
+//Login Controller
+//read the request username and password
+//Find the user associated w username
+//Hash the given password and compare against truth
+
+func loginUser(resp http.ResponseWriter, req *http.Request) {
+	if req.Header.Get("Content-Type") != "application/json" {
+		resp.WriteHeader(400)
+		resp.Write([]byte("400 Bad Request"))
+		return
+	}
+
+	//read in the raw request body
+	//deseralize it into a user struct
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		resp.WriteHeader(400)
+		resp.Write([]byte("400 Bad Request"))
+		return
+	}
+
+	var user User
+	err = json.Unmarshal(body, &user)
+	if err != nil || len(user.Username) == 0 || len(user.Password) == 0 {
+		resp.WriteHeader(400)
+		resp.Write([]byte("400 Bad Request"))
+		return
+	}
+
+	coll := database.Database.Collection("users")
+	var dbUser User
+	filter := bson.D{{"username", user.Username}}
+	err = coll.FindOne(context.TODO(), filter).Decode(&dbUser)
+	if err != nil {
+		resp.WriteHeader(404)
+		resp.Write([]byte("404 Not Found"))
+		return
+	}
+
+	key := []byte(os.Getenv("SECRET_KEY"))
+	hmacEnc := hmac.New(sha256.New, key)
+	hmacEnc.Write([]byte(user.Password))
+	hashedPassword := hex.EncodeToString(hmacEnc.Sum(nil))
+
+	if hashedPassword != dbUser.Password {
+		resp.WriteHeader(401)
+		resp.Write([]byte("401 Unauthorized"))
+		return
+	}
+
+	resp.Write([]byte("OK"))
+}
+
 func init() {
 	AuthRouter = chi.NewRouter()
 	AuthRouter.Post("/register", registerUser)
+	AuthRouter.Post("/login", loginUser)
 }
