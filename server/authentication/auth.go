ea866b5015cc549d33a96ad3683d3dbaee90ab86
kimcchen <kimberlykchen@gmail.com>
2025-01-21 20:10:06 -0600

    added token authentication

--- server/authentication/auth.go
+++ server/authentication/auth.go
@@ -13,6 +13,11 @@ import (
 
 	"github.com/go-chi/chi"
 	"go.mongodb.org/mongo-driver/bson"
+
+	"github.com/golang-jwt/jwt/v5"
+	"time"
+	"fmt"
+	"strings"
 )
 
 type User struct {
@@ -21,6 +26,7 @@ type User struct {
 }
 
 var AuthRouter *chi.Mux
+var key = []byte(os.Getenv("SECRET_KEY"))
 
 func registerUser(resp http.ResponseWriter, req *http.Request) {
 	if req.Header.Get("Content-Type") != "application/json" {
@@ -46,7 +52,7 @@ func registerUser(resp http.ResponseWriter, req *http.Request) {
 	}
 
 	//hash password
-	key := []byte(os.Getenv("SECRET_KEY"))
+	// key := []byte(os.Getenv("SECRET_KEY"))
 	hmacEnc := hmac.New(sha256.New, key)
 	hmacEnc.Write([]byte(user.Password))
 	user.Password = hex.EncodeToString(hmacEnc.Sum(nil))
@@ -69,6 +75,14 @@ func registerUser(resp http.ResponseWriter, req *http.Request) {
 //Find the user associated w username
 //Hash the given password and compare against truth
 
+/**
+ * loginUser - Reads in User information, authorizes and authenticates User.
+ *
+ * @resp - The HTTP response writer used to send the response to the client.
+ * @req - The HTTP request containing the incoming data, such as Username and Password.
+ *
+ * @return: None.
+ */
 func loginUser(resp http.ResponseWriter, req *http.Request) {
 	if req.Header.Get("Content-Type") != "application/json" {
 		resp.WriteHeader(400)
@@ -103,7 +117,7 @@ func loginUser(resp http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	key := []byte(os.Getenv("SECRET_KEY"))
+	// key := []byte(os.Getenv("SECRET_KEY"))
 	hmacEnc := hmac.New(sha256.New, key)
 	hmacEnc.Write([]byte(user.Password))
 	hashedPassword := hex.EncodeToString(hmacEnc.Sum(nil))
@@ -114,11 +128,116 @@ func loginUser(resp http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	resp.Write([]byte("OK"))
+	// now that we have authenticated, we need to create our token 
+	tokenString, err := generateToken(user.Username)
+	if err != nil {
+		resp.WriteHeader(404)
+		resp.Write([]byte("404 Not Found"))
+		return
+	}
+	// resp.Write([]byte("OK"))
+	resp.Write([]byte(tokenString)) // writing token to response
+}
+
+// first, we need to create a function that generates our jwt token with our secret key
+// then, when our user logs in, that jwt token will be sent over to the client side
+// when our client tries to access a specific page/route again, our server will authorize it by verifying the jwt signature
+
+/**
+ * generateToken - Creates a new JWT (Java Web Token).
+ *
+ * @username: Username of client.
+ *
+ * @return: Returns the signed token or error.
+ */
+func generateToken(username string) (string, error) {
+	// key := []byte(os.Getenv("SECRET_KEY"))
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"username": username,
+			"exp": jwt.NewNumericDate(time.Now().Add(7 * 24 * time.Hour)), // temporarily, I just set exp date for a week from today (Jan 28th)
+		})
+
+	ss, err := token.SignedString(key)
+	if err != nil {
+		return "", err
+	}
+
+	return ss, nil
+}
+
+/**
+ * verifyToken - Verifies that the JWT is valid.
+ *
+ * @tokenString - The JWT that is being verified.
+ *
+ * @return: None, unless error.
+ */
+func verifyToken(tokenString string) (error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			fmt.Println("Unexpected signing method")
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return key, nil
+	})
+
+	if err != nil {
+		fmt.Println("Error parsing token: ", err)
+		return err
+	}
+
+	if !token.Valid {
+		err := fmt.Errorf("invalid token")
+    	fmt.Println("Error:", err)
+		return fmt.Errorf("invalid token")
+	}
+	// will need to add more switch cases 
+
+	return nil
+}
+
+/**
+ * testingHandler - Creates a route that is used to test whether JWT Authentication works as intended. Temporary.
+ *
+ * @resp - The HTTP response writer used to send the response to the client.
+ * @req - The HTTP request containing the incoming data, such as the Authentication Token.
+ *
+ * @return: None.
+ */
+func testingHandler(resp http.ResponseWriter, req *http.Request) {
+	if req.Header.Get("Content-Type") != "application/json" {
+		resp.WriteHeader(400)
+		resp.Write([]byte("400 Bad Request"))
+		return
+	}
+	tokenString := req.Header.Get("Authorization");
+	// this is lowkey monkeycode, need to figure out
+	parts := strings.Split(tokenString, " ")
+	token := parts[1]
+	if strings.HasPrefix(token, "OK") {
+		token = token[2:]
+	}
+	// fmt.Println("TOKEN STRING: ", token)
+	if token == "" {
+		resp.WriteHeader(500)
+		resp.Write([]byte("500 Internal Server Error"))
+		return
+	}
+	err := verifyToken(token)
+	if err != nil {
+		resp.WriteHeader(401)
+		resp.Write([]byte("401 Unauthorized and token not verified"))
+		return
+	}
+
+	resp.Write([]byte("Welcome"))
 }
 
 func init() {
 	AuthRouter = chi.NewRouter()
 	AuthRouter.Post("/register", registerUser)
 	AuthRouter.Post("/login", loginUser)
+	AuthRouter.Get("/testing", testingHandler)
 }
--- server/go.mod
+++ server/go.mod
@@ -5,6 +5,7 @@ go 1.23.2
 require github.com/go-chi/chi v1.5.5
 
 require (
+	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/klauspost/compress v1.16.7 // indirect
